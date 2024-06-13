cdc76ad51103b612aa928edab5570e800d716ce6
Muhammad Yudhistira <myudhistira07@gmail.com>
2024-06-13 22:47:28 +0700

    finished

---
+++ app/meals/share/error.jsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+const error = ({ error }) => {
+    return (
+        <main className='error'>
+            <h1>An Error occurred!</h1>
+            <p>Failed to create meal.</p>
+        </main>
+    )
+}
+
+export default error
